13b42d5171447cfa97ee4636fd28aa293ac12dcc
Mahdi Aryayi <mahdiaryayi@gmail.com>
2018-02-18 12:35:55 +0330

    Added copy button to project

---
+++ copy.js
@@ -0,0 +1,12 @@
+var clipboard = new Clipboard('.copy');
+clipboard.on('success', function(e) {
+    document.getElementById('default').style.display = "none";
+    document.getElementById('done').style.display = "inline";
+});
+clipboard.on('error', function(e) {
+    console.log(e);
+});
+document.getElementById('go').addEventListener('click', function() {
+    document.getElementById('default').style.display = "inline";
+    document.getElementById('done').style.display = "none";
+});
